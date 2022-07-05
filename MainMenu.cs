a93a59a39ee9501575eda767460c1e8e040b0c06
damibran <olegevdk@gmail.com>
2022-07-05 20:26:59 +0300

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -7,9 +7,6 @@ public class MainMenu : MarginContainer
     [Export]
     private NodePath menu_root_node_path;
     private Control menu_root_node;
-
-    private 
-
     public override void _Ready()
     {
         menu_root_node = GetNode<Control>(menu_root_node_path);
